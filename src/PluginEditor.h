bb9881a1589b78c6921977c0659f66805ff29818
Jens Lindahl <jensctl@gmail.com>
2019-05-24 16:55:18 +0200

    Add placeholder source Add auto-generated headers

--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -1,14 +1,35 @@
-//
-// Created by Jens Lindahl on 2019-05-24.
-//
+/*
+  ==============================================================================
 
-#ifndef DAFXJUCE_PLUGINEDITOR_H
-#define DAFXJUCE_PLUGINEDITOR_H
+    This file was auto-generated!
 
+    It contains the basic framework code for a JUCE plugin editor.
 
-class PluginEditor {
+  ==============================================================================
+*/
 
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class NewProjectAudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    NewProjectAudioProcessorEditor (NewProjectAudioProcessor&);
+    ~NewProjectAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    NewProjectAudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NewProjectAudioProcessorEditor)
 };
-
-
-#endif //DAFXJUCE_PLUGINEDITOR_H
