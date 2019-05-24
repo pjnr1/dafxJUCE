e7cf99a0408c7c499d8f9e9daddf65f4f784f6ee
Jens Lindahl <jensctl@gmail.com>
2019-05-24 17:16:19 +0200

    Add proper flags for Avid SDK paths Add Escape methods if paths not found

--- cmake/AddAudioPluginModules.cmake
+++ cmake/AddAudioPluginModules.cmake
@@ -1,32 +1,40 @@
 
 function(AddAudioPluginModules)
-
     # -- VST3
-    add_library (VST3 MODULE
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp"
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_VST3.cpp"
-            "${PROJECT_SOURCE_DIR}/PkgInfo"
-            )
+    if(BUILD_VST3)
+        add_library (VST3 MODULE
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp"
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_VST3.cpp"
+                "${PROJECT_SOURCE_DIR}/PkgInfo"
+                )
 
 
-    set_source_files_properties ("${PROJECT_SOURCE_DIR}/PkgInfo" PROPERTIES MACOSX_PACKAGE_LOCATION .)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp" PROPERTIES HEADER_FILE_ONLY TRUE)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_VST3.cpp" PROPERTIES HEADER_FILE_ONLY TRUE)
+        set_source_files_properties ("${PROJECT_SOURCE_DIR}/PkgInfo" PROPERTIES MACOSX_PACKAGE_LOCATION .)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp" PROPERTIES HEADER_FILE_ONLY TRUE)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_VST3.cpp" PROPERTIES HEADER_FILE_ONLY TRUE)
+    endif()
 
     # -- AAX
-    add_library (AAX MODULE
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/AAX/juce_AAX_Modifier_Injector.h"
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/AAX/juce_AAX_Wrapper.cpp"
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AAX.cpp"
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AAX.mm"
-            "${PROJECT_SOURCE_DIR}/PkgInfo"
-            )
+    if (BUILD_AAX)
 
-    set_source_files_properties ("${PROJECT_SOURCE_DIR}/PkgInfo" PROPERTIES MACOSX_PACKAGE_LOCATION .)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/AAX/juce_AAX_Modifier_Injector.h" PROPERTIES HEADER_FILE_ONLY TRUE)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/AAX/juce_AAX_Wrapper.cpp" PROPERTIES HEADER_FILE_ONLY TRUE)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AAX.cpp" PROPERTIES HEADER_FILE_ONLY TRUE)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AAX.mm" PROPERTIES HEADER_FILE_ONLY TRUE)
+        if(COMMAND cmake_policy)
+            cmake_policy(SET CMP0003 NEW)
+        endif(COMMAND cmake_policy)
+
+        add_library (AAX MODULE
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/AAX/juce_AAX_Modifier_Injector.h"
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/AAX/juce_AAX_Wrapper.cpp"
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AAX.cpp"
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AAX.mm"
+                "${PROJECT_SOURCE_DIR}/PkgInfo"
+                )
+
+        set_source_files_properties ("${PROJECT_SOURCE_DIR}/PkgInfo" PROPERTIES MACOSX_PACKAGE_LOCATION .)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/AAX/juce_AAX_Modifier_Injector.h" PROPERTIES HEADER_FILE_ONLY TRUE)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/AAX/juce_AAX_Wrapper.cpp" PROPERTIES HEADER_FILE_ONLY TRUE)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AAX.cpp" PROPERTIES HEADER_FILE_ONLY TRUE)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AAX.mm" PROPERTIES HEADER_FILE_ONLY TRUE)
+    endif ()
 
     # -- RTAS
     if(BUILD_RTAS)
@@ -65,20 +73,21 @@ function(AddAudioPluginModules)
     endif()
 
     # -- AU
-    add_library (AU MODULE
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/AU/juce_AU_Wrapper.mm"
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU.r"
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU_1.mm"
-            "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU_2.mm"
-            "${JUCE_MODULES_PATH}/juce_audio_processors/format_types/juce_AU_Shared.h"
-            "${PROJECT_SOURCE_DIR}/PkgInfo"
-            )
+    if(BUILD_AU)
+        add_library (AU MODULE
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/AU/juce_AU_Wrapper.mm"
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU.r"
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU_1.mm"
+                "${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU_2.mm"
+                "${JUCE_MODULES_PATH}/juce_audio_processors/format_types/juce_AU_Shared.h"
+                "${PROJECT_SOURCE_DIR}/PkgInfo"
+                )
 
-    set_source_files_properties ("${PROJECT_SOURCE_DIR}/PkgInfo" PROPERTIES MACOSX_PACKAGE_LOCATION .)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/AU/juce_AU_Wrapper.mm" PROPERTIES HEADER_FILE_ONLY TRUE)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU_1.mm" PROPERTIES HEADER_FILE_ONLY TRUE)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU_2.mm" PROPERTIES HEADER_FILE_ONLY TRUE)
-    set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_processors/format_types/juce_AU_Shared.h" PROPERTIES HEADER_FILE_ONLY TRUE)
-
+        set_source_files_properties ("${PROJECT_SOURCE_DIR}/PkgInfo" PROPERTIES MACOSX_PACKAGE_LOCATION .)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/AU/juce_AU_Wrapper.mm" PROPERTIES HEADER_FILE_ONLY TRUE)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU_1.mm" PROPERTIES HEADER_FILE_ONLY TRUE)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_plugin_client/juce_audio_plugin_client_AU_2.mm" PROPERTIES HEADER_FILE_ONLY TRUE)
+        set_source_files_properties ("${JUCE_MODULES_PATH}/juce_audio_processors/format_types/juce_AU_Shared.h" PROPERTIES HEADER_FILE_ONLY TRUE)
+    endif()
 
 endfunction()
