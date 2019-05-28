d96e4fa48c9a6060dfd1f12c1a6dbbb78f5f2fd6
Jens Lindahl <jensctl@gmail.com>
2019-05-28 13:59:06 +0200

    Correct plist

--- cmake/SetAppVersion.cmake
+++ cmake/SetAppVersion.cmake
@@ -1,10 +1,13 @@
 function (SetAppVersion major minor patch)
     GetHexVersion(${major} ${minor} ${patch} hex)
 
-    message("Version:\t${major}.${minor}.${patch} (${hex})")
+    math(EXPR dec "${hex}" OUTPUT_FORMAT DECIMAL)
+
+    message("Version:\t${major}.${minor}.${patch} (${hex}) (${dec})")
 
     set(PLUGIN_MAJOR_VERSION ${major} PARENT_SCOPE)
     set(PLUGIN_MINOR_VERSION ${minor} PARENT_SCOPE)
     set(PLUGIN_PATCH_VERSION ${patch} PARENT_SCOPE)
     set(PLUGIN_HEX_VERSION ${hex} PARENT_SCOPE)
+    set(PLUGIN_DEC_VERSION ${dec} PARENT_SCOPE)
 endfunction()
